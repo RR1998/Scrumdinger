55857c35c7edaf65e99b7a9d1c227e2452f417ad
Roberto Github <roberto_rubio98@outlook.com>
2025-02-17 22:31:44 -0600

    :sparkles: Add dynamic list text

---
+++ FirstProject/FirstProject/Models/History.swift
@@ -0,0 +1,19 @@
+//
+//  History.swift
+//  FirstProject
+//
+//  Created by Roberto Rubio on 17/2/25.
+//
+import Foundation
+
+struct History: Identifiable {
+    let id: UUID
+    let date: Date
+    var attendees: [DailyScrum.Attendee]
+    
+    init(id: UUID = UUID(), date: Date = Date(), attendees: [DailyScrum.Attendee]) {
+        self.id = id
+        self.date = date
+        self.attendees = attendees
+    }
+}
